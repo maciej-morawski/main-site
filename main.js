4613ac50109dfc0c249d6b0ff0dbab4a4805d26d
Maciek Morawski <murek112@gmail.com>
2022-01-15 22:16:09 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+console.log("Witam Cie serdecznie na mojej stronie");
+
+const age = 29;
+const firstName = "Maciej";
+
+console.log(firstName);
+console.log(age);
+console.log(`Nazywam sie ${firstName} i mam ${age} lat.`);
